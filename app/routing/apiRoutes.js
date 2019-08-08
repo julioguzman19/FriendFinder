095ae9059ae94f14d3d384e09fbd327eb13732f6
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-08 10:45:51 -0600

    test data added with 3 people

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/survey", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
@@ -29,7 +29,7 @@ module.exports = function(app) {
   // Then the server saves the data to the friends array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
